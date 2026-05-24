d00e0bb124dc2394da13f58b34379b18800d23ec
joshiyj <yashjoshi7972@gmail.com>
2026-05-24 18:56:46 +0530

    Update Axios base URL to point to Singapore backend

--- hospital-frontend/src/api/axios.js
+++ hospital-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hospital-backend-mhks.onrender.com",
+  baseURL: "https://hospital-backend-rl7j.onrender.com",
 });
 
 export default API;
